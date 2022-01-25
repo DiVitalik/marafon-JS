9a3d4e3f4317c1f6743836ea033125b8b8903cf9
Vitaliy <vitalik.unit@gmail.com>
2022-01-26 00:49:30 +0500

    done

--- calc.js
+++ calc.js
@@ -1,13 +1,13 @@
 
 function checkInput(num1,num2) {
-    let checkInput = (typeof num1 !== 'number' || typeof num2 !== 'number')
+    let checkInput = (typeof num1 !== 'number' && typeof num2 !== 'number')
     if (checkInput) {
         return true;
     }
 }
 
 function calc(Operatoin,num1,num2){
-    if (checkInput(num1,num2)) return 'Is not text';
+    if (checkInput(num1,num2)) return 'Is not number';
     switch (Operatoin) {
         case "add":
             return num1+num2;
@@ -25,7 +25,7 @@ function calc(Operatoin,num1,num2){
 
 function calcObject(operation,num1,num2){
 
-    if (checkInput(num1,num2)) return 'Is not text';
+    if (checkInput(num1,num2)) return 'Is not number';
 
     const operations = {
         add: num1+num2,
@@ -34,26 +34,19 @@ function calcObject(operation,num1,num2){
         div: (num1===0) ? "division by zero" : num1/num2
     }
 
-       for ([operation] in operations) {
-          return operations[operation];
-       }
-    // const findOperator = ([operation] in operations);
-    //
-    // if (findOperator) {
-    //     return  operations[operation];
-    // }
-    return "unknown operation";
+    const findOperator = ([operation] in operations);
 
+    if (findOperator)
+        return  operations[operation];
+    else
+        return "unknown operation";
 }
 
 console.log("---------------Switch---------------")
-console.log(calc('add', "35",3));
-console.log(calc('sub', 3,3));
-console.log(calc('div', 3,3));
+console.log(calc('ааа', "35",3));
+console.log(calc());
 
 console.log("---------------Object---------------")
-
-console.log(calcObject('add', 10,3));
-console.log(calcObject('sub', 2,3));
-console.log(calcObject('mul', 4,3));
-console.log(calcObject('div', 0,3));
+console.log(calcObject('add', "10","3"));
+console.log(calcObject('add',2,3));
+console.log(calcObject());
